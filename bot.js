277d901131569d50bbd68a56a2dcad24342c315e
Lola Fakinlede <lfakinlede@covenanthousebc.org>
2022-05-13 09:18:09 -0700

    feat: Changes to bot app

--- bot.js
+++ bot.js
@@ -4,55 +4,78 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
 const { QnAMaker } = require('botbuilder-ai');
-const DentistScheduler = require('./dentistscheduler');
+const DentistScheduler = require('./dentistScheduler');
 const IntentRecognizer = require("./intentrecognizer")
 
 class ldotBot extends ActivityHandler {
     constructor(configuration, qnaOptions) {
-        // call the parent constructor
         super();
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
-
+        
         // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker()
-       
+        this.qnaMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions);
         // create a DentistScheduler connector
-      
-        // create a IntentRecognizer connector
+        this.dentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration);
 
+        // create a LUIS connector
+        this.intentRecognizer = new IntentRecognizer(configuration.LuisConfiguration);
 
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            // send user input to QnA Maker and collect the response in a variable
-            // don't forget to use the 'await' keyword
-          
-            // send user input to IntentRecognizer and collect the response in a variable
-            // don't forget 'await'
-                     
-            // determine which service to respond with based on the results from LUIS //
+            // Send user input to QnA Maker
+            const qnaResults = await this.qnaMaker.getAnswers(context);
+            // Send user input to LUIS
+            const LuisResult = await this.intentRecognizer.executeLuisQuery(context);
+            
+            // Determine which service to respond with //
+            if (LuisResult.luisResult.prediction.topIntent === "getAvailability" &&
+                LuisResult.intents.getAvailability.score > .6 &&
+                LuisResult.entities.$instance && 
+                LuisResult.entities.$instance.timeday  && 
+                LuisResult.entities.$instance.timeday[0]
+            ) {
+                const timeday = LuisResult.entities.$instance.timeday[0].text;
+                // Call api with time entity info
+                const getAvailableTime = "I have a few spots for " + timeday;
+                console.log(getAvailableTime)
+                await context.sendActivity(getAvailableTime);
+                await next();
+                return;
+            } 
 
-            // if(top intent is intentA and confidence greater than 50){
-            //  doSomething();
-            //  await context.sendActivity();
-            //  await next();
-            //  return;
-            // }
-            // else {...}
-             
+            // If an answer was received from QnA Maker, send the answer back to the user.
+            if (qnaResults[0]) {
+                console.log(qnaResults[0])
+                await context.sendActivity(`${qnaResults[0].answer}`);
+            }
+            else {
+                // If no answers were returned from QnA Maker, reply with help.
+                await context.sendActivity(`I'm not sure`
+                + ' I found an answer to your question. '
+                + `You can ask me questions about our dental services like "Do you treat children?' or tell me what time you want an appointment like "today"`);
+            }
             await next();
-    });
+        });
+
+
+        /*this.onMessage(async (context, next) => {
+            const replynpmheText = `Echo: ${ context.activity.text }`;
+            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });*/
 
         this.onMembersAdded(async (context, next) => {
-        const membersAdded = context.activity.membersAdded;
-        //write a custom greeting
-        const welcomeText = 'Welcome. I am the L.Contoso Assistant Chatbot. I can try to answer a health question you have or we can set up an appointment for you to see a dentist. Go ahead and ask me a dental health question or say "book an appointment".';
-        for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-            if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+            const membersAdded = context.activity.membersAdded;
+            const welcomeText = 'Welcome. I am ldot. Do you have a question about our services? Or I can book you a session with a dentist. You can ask me a dental health question like "what appointments are available" or "book tomorrow".';
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                }
             }
-        }
-        // by calling next() you ensure that the next BotHandler is run.
-        await next();
-    });
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
     }
 }
 
--- intentrecognizer.js
+++ intentrecognizer.js
@@ -25,18 +25,14 @@ class IntentRecognizer {
     async executeLuisQuery(context) {
         return await this.recognizer.recognize(context);
     }
-
- 
+    
     getTimeEntity(result) {
-        const datetimeEntity = result.entities.datetime;
-        if (!datetimeEntity || !datetimeEntity[0]) return undefined;
-
-        const timex = datetimeEntity[0].timex;
-        if (!timex || !timex[0]) return undefined;
-
-        const datetime = timex[0]
-        return datetime;
+        const timeEntity = result.entities.timeday;
+        if (!timeEntity || !timeEntity[0]) return undefined;
+        return time;
     }
+
+
 }
 
 module.exports = IntentRecognizer
